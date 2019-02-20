265c6a85b9756970988b8cf8ebfa18c76e264926
GinjaNinja32 <ginjaninja32@gmail.com>
2019-02-20 00:22:06 +0000

    remove debug code

--- mod_dice/src/dice.rs
+++ mod_dice/src/dice.rs
@@ -1,33 +1,3 @@
-/*
-#[derive(Debug, PartialEq)]
-pub struct Color {
-    pub red: u8,
-    pub green: u8,
-    pub blue: u8,
-}
-
-fn from_hex(input: &str) -> Result<u8, std::num::ParseIntError> {
-    u8::from_str_radix(input, 16)
-}
-
-fn is_hex_digit(c: char) -> bool {
-    c.is_digit(16)
-}
-
-named!(hex_primary<&str, u8>,
-  map_res!(take_while_m_n!(2, 2, is_hex_digit), from_hex)
-);
-
-named!(hex_color<&str, Color>,
-    do_parse!(
-               tag!("#")   >>
-        red:   hex_primary >>
-        green: hex_primary >>
-        blue:  hex_primary >>
-        (Color { red, green, blue })
-    )
-);
-*/
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 use std::fmt;
@@ -35,14 +5,9 @@ use std::fmt::Display;
 use std::iter;
 
 pub fn parse(input: &str) -> Result<Expression, String> {
-    let res = fullexpr(&format!("{}\n", input))
+    fullexpr(&format!("{}\n", input))
         .map(|(_, c)| c)
-        .map_err(|e| {
-            std::dbg!(&e);
-            format!("{:?}", e)
-        });
-    std::dbg!(&res);
-    res
+        .map_err(|e| format!("{:?}", e))
 }
 
 pub fn eval(expr: Expression) -> Result<String, String> {
