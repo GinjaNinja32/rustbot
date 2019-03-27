831453c85ef21a104950dcc8d7579e2bc0326b03
GinjaNinja32 <ginjaninja32@gmail.com>
2019-03-27 13:50:37 +0000

    extend timeouts and allow rsc in secure mode

--- scripts/dm_compile_run.sh
+++ scripts/dm_compile_run.sh
@@ -8,12 +8,20 @@ format_out() {
 	fi
 }
 
-timeout="$HOME/timeout --just-kill --no-info-on-success --detect-hangups -h 10 -t 10 -m 102400"
+if [[ "$secure" == "true" ]]; then
+	timeout="$HOME/timeout --just-kill --no-info-on-success --detect-hangups -h 600 -t 600"
+else
+	timeout="$HOME/timeout --just-kill --no-info-on-success --detect-hangups -h 10 -t 10 -m 102400"
+fi
 
 file=$1
 
 if [[ $2 == "run" ]]; then
-	DreamDaemon "$file.dmb" -invisible -ultrasafe 2>&1 | tail -n +4 | head -c 512
+	if [[ "$secure" == "true" ]]; then
+		DreamDaemon "$file.dmb" -invisible -safe 2>&1 | tail -n +4 | head -c 512
+	else
+		DreamDaemon "$file.dmb" -invisible -ultrasafe 2>&1 | tail -n +4 | head -c 512
+	fi
 	exit 0
 fi
 
@@ -29,7 +37,7 @@ return=$?
 if [[ $return != 0 ]]; then
 	echo "$output" | tail -n +3 | format_out
 else
-	if [[ -e $file.rsc ]]; then
+	if [[ "$secure" != "true" && -e $file.rsc ]]; then
 		echo "You attempted to use a resource file; this is blocked for security reasons."
 	else
 		$timeout "../$0" "$file" run | format_out
