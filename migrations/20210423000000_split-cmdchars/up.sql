48793fb5b04c7a2f77de86706649136ea1e848f6
GinjaNinja32 <ginjaninja32@gmail.com>
2021-04-23 18:00:33 +0100

    add missing foreign key constraint

--- migrations/20210423000000_split-cmdchars/up.sql
+++ migrations/20210423000000_split-cmdchars/up.sql
@@ -3,7 +3,8 @@ CREATE TABLE cmdchars (
 	channel TEXT NOT NULL,
 	cmdchars TEXT NOT NULL,
 
-	PRIMARY KEY (config_id, channel)
+	PRIMARY KEY (config_id, channel),
+	CONSTRAINT fk_config FOREIGN KEY (config_id) REFERENCES configs(id)
 );
 INSERT INTO cmdchars (config_id, channel, cmdchars)
 	SELECT id, '%', cmdchars FROM configs;
